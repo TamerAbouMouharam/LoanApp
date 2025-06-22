3ace5c3a76ba6bfecfad7ca863abacc013f55c1c
unknown <tamer12465@gmail.com>
2025-06-22 19:28:33 +0300

    Refactored

--- LoanApp/LoanEvaluator.cs
+++ LoanApp/LoanEvaluator.cs
@@ -2,41 +2,51 @@ namespace LoanApp;
 
 public class LoanEvaluator
 {
+    public string CheckDependents(int dependents)
+    {
+        if (dependents == 0)
+            return "Eligible";
+        else if (dependents <= 2)
+            return "Review Manually";
+        else
+            return "Not Eligible";
+    }
+
+    public string CheckOwningHouse(bool ownsHouse)
+    {
+        if (ownsHouse)
+            return "Review Manually";
+        else
+            return "Not Eligible";
+    }
+
+    public string CheckHavingJob(int creditScore, int dependents, bool ownsHouse)
+    {
+        if (creditScore >= 700)
+            return CheckDependents(dependents);
+        else if (creditScore >= 600)
+            return CheckOwningHouse(ownsHouse);
+        else
+            return "Not Eligible";
+    }
+
+    public string CheckIfNotHaveJob(int income, int creditScore, int dependents, bool ownsHouse)
+    {
+        if (creditScore >= 750 && income > 5000 && ownsHouse)
+            return "Eligible";
+        else if (creditScore >= 650 && dependents == 0)
+            return "Review Manually";
+        else
+            return "Not Eligible";
+    }
+
     public string GetLoanEligibility(int income, bool hasJob, int creditScore, int dependents, bool ownsHouse)
     {
         if (income < 2000)
             return "Not Eligible";
-        if (hasJob)
-        {
-            if (creditScore >= 700)
-            {
-                if (dependents == 0)
-                    return "Eligible";
-                else if (dependents <= 2)
-                    return "Review Manually";
-                else
-                    return "Not Eligible";
-            }
-            else if (creditScore >= 600)
-            {
-                if (ownsHouse)
-                    return "Review Manually";
-                else
-                    return "Not Eligible";
-            }
-            else
-            {
-                return "Not Eligible";
-            }
-        }
+        else if (hasJob)
+            return CheckHavingJob(creditScore, dependents, ownsHouse);
         else
-        {
-            if (creditScore >= 750 && income > 5000 && ownsHouse)
-                return "Eligible";
-            else if (creditScore >= 650 && dependents == 0)
-                return "Review Manually";
-            else
-                return "Not Eligible";
-        }
+            return CheckIfNotHaveJob(income, creditScore, dependents, ownsHouse);
     }
 }
